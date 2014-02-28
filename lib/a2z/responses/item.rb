befe4f4634840c19eb8da02b068a0dc9d4f2781f
Luke Williams <luke@red-root.com>
2014-02-28 21:29:50 +0000

    added ability to have multi item lookup

--- lib/a2z/responses/item.rb
+++ lib/a2z/responses/item.rb
@@ -48,45 +48,49 @@ module A2z
       end
       
       def self.from_response(data)
-        new.tap do |item|
-          item.asin = data['ASIN']
-          item.parent_asin = data['ParentASIN']
-          item.detail_page_url = data['DetailPageURL']
-          
-          item.small_image = Image.from_response(data['SmallImage']) if data['SmallImage']
-          item.medium_image = Image.from_response(data['MediumImage']) if data['MediumImage']
-          item.large_image = Image.from_response(data['LargeImage']) if data['LargeImage']
-          
-          item.offer_summary = OfferSummary.from_response(data['OfferSummary']) if data['OfferSummary']
-          
-          if data['ItemLinks'] && data['ItemLinks']['ItemLink']
-            item_links = array_wrap(data['ItemLinks']['ItemLink'])
-            item.links = item_links.collect { |link| ItemLink.from_response(link) }
-          end
-          
-          if data['ItemAttributes']
-            data['ItemAttributes'].each { |key, value| item[underscore(key)] = value }
-          end
-          
-          if data['ImageSets'] && data['ImageSets']['ImageSet']
-            image_sets = array_wrap(data['ImageSets']['ImageSet'])
-            image_sets.each do |image_set|
-              image_set = ImageSet.from_response(image_set)
-              item.image_sets[image_set.category.to_sym] = image_set
+        if data.kind_of?(Array)
+          return data.map { |item| from_response(item) }
+        else
+          new.tap do |item|
+            item.asin = data['ASIN']
+            item.parent_asin = data['ParentASIN']
+            item.detail_page_url = data['DetailPageURL']
+            
+            item.small_image = Image.from_response(data['SmallImage']) if data['SmallImage']
+            item.medium_image = Image.from_response(data['MediumImage']) if data['MediumImage']
+            item.large_image = Image.from_response(data['LargeImage']) if data['LargeImage']
+            
+            item.offer_summary = OfferSummary.from_response(data['OfferSummary']) if data['OfferSummary']
+            
+            if data['ItemLinks'] && data['ItemLinks']['ItemLink']
+              item_links = array_wrap(data['ItemLinks']['ItemLink'])
+              item.links = item_links.collect { |link| ItemLink.from_response(link) }
             end
+            
+            if data['ItemAttributes']
+              data['ItemAttributes'].each { |key, value| item[underscore(key)] = value }
+            end
+            
+            if data['ImageSets'] && data['ImageSets']['ImageSet']
+              image_sets = array_wrap(data['ImageSets']['ImageSet'])
+              image_sets.each do |image_set|
+                image_set = ImageSet.from_response(image_set)
+                item.image_sets[image_set.category.to_sym] = image_set
+              end
+            end
+            
+            if data['EditorialReviews'] && data['EditorialReviews']['EditorialReview']
+              reviews = array_wrap(data['EditorialReviews']['EditorialReview'])
+              item.editorial_reviews = reviews.collect { |review| EditorialReview.from_response(review) }
+            end
+            
+            if data['Offers'] && data['Offers']['Offer']
+              offers = array_wrap(data['Offers']['Offer'])
+              item.offers = offers.collect { |offer| Offer.from_response(offer) }
+            end
+            
+            item.freeze
           end
-          
-          if data['EditorialReviews'] && data['EditorialReviews']['EditorialReview']
-            reviews = array_wrap(data['EditorialReviews']['EditorialReview'])
-            item.editorial_reviews = reviews.collect { |review| EditorialReview.from_response(review) }
-          end
-          
-          if data['Offers'] && data['Offers']['Offer']
-            offers = array_wrap(data['Offers']['Offer'])
-            item.offers = offers.collect { |offer| Offer.from_response(offer) }
-          end
-          
-          item.freeze
         end
       end
     end
